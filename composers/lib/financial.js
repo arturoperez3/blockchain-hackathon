f6fa75c797aa26dff03a6f3650980d18050340fc
Arturo <arturo.p3rez@gmail.com>
2017-09-24 02:00:38 -0500

    Adding js script

--- composers/lib/financial.js
+++ composers/lib/financial.js
@@ -22,50 +22,54 @@
      * @param {composers.financial.SendBill} newBill - the SendBill transaction
      * @transaction
      */
-function SendBill(newBill) {
-    var balanceDue = newBill.bill.amount;
-  
-  	var ID = newBill.bill.patientID;
-  	console.log("HELLO");
-    return getParticipantRegistry('composers.participants.Patient')
-        .then(function(patientRegistry) {
-      		console.log("OK");
-      		return patientRegistry.get(ID).then(function(patient){
-            	console.log("BBB");
-      			patient.balanceDue += newBill.bill.amount;
-     			newBill.bill.paid = false;
-     			return patientRegistry.update(patient);
-            })
-    	})
-}
- /**
-     * Place an order for a vehicle
-     * @param {composers.financial.PayBill} oldBill - the PayBill transaction
-     * @transaction
-     */
-function PayBill(oldBill) {
-    var balancePaid = oldBill.bill.amount;
-  
-  	var ID = oldBill.bill.patientID;
-  	console.log("HELLO");
-    return getAssetRegistry('composers.financial.HospitalMoneyPool')
-        .then(function(assetRegistry) {
-      		console.log("OK");
-      		return assetRegistry.get(ID).then(function(moneypool){
-            	console.log("BBB");
-              	HospitalMoneyPool.moneypool += oldBill.bill.amount;
-     			oldBill.bill.paid = true;
-     			return patientRegistry.update(patient);
-            })
-    	})
-    	.then(function(){getParticipantRegistry('composers.participants.Patient')
+    function SendBill(newBill) {
+        var balanceDue = newBill.bill.amount;
+      
+          var ID = newBill.bill.patientID;
+          console.log("HELLO");
+        return getParticipantRegistry('composers.participants.Patient')
             .then(function(patientRegistry) {
-                console.log("OK");
-                return patientRegistry.get(ID).then(function(patient){
+                  console.log("OK");
+                  return patientRegistry.get(ID).then(function(patient){
                     console.log("BBB");
-                    patient.balanceDue -= oldBill.bill.amount;
-                    return patientRegistry.update(patient);
+                      patient.balanceDue += newBill.bill.amount;
+                     newBill.bill.paid = false;
+                     return patientRegistry.update(patient);
                 })
             })
-         })
-}
+    }
+     /**
+         * Place an order for a vehicle
+         * @param {composers.financial.PayBill} oldBill - the PayBill transaction
+         * @transaction
+         */
+    function PayBill(oldBill) {
+        //var balancePaid = oldBill.bill.amount;
+      
+          var ID = oldBill.bill.patientID;
+          var moneyID = oldBill.bill.moneyID;
+          var amt = oldBill.bill.amount;
+          console.log("HELLO");
+        return getAssetRegistry('composers.financial.HospitalMoneyPool')
+            .then(function(assetRegistry) {
+                  console.log("OK");
+                  return assetRegistry.get(moneyID).then(function(_moneypool){
+                    console.log("BBB");
+                      _moneypool.moneypool += amt;
+                     oldBill.bill.paid = true;
+                     return assetRegistry.update(_moneypool);
+                  
+                })
+            })
+            .then(function(){getParticipantRegistry('composers.participants.Patient')
+                .then(function(patientRegistry) {
+                    console.log("OK");
+                    return patientRegistry.get(ID).then(function(patient){
+                        console.log("BBB");
+                        patient.balanceDue -= amt;
+                        return patientRegistry.update(patient);
+                    })
+                })
+             })
+             
+    }
